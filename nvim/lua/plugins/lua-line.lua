060a254c03dc30cb6caf617dbbd9fa5ef1c898a2
Kavinthe Perera <kavintheperera@gmail.com>
2024-05-22 14:32:35 +0530

    global lua line

--- nvim/lua/plugins/lua-line.lua
+++ nvim/lua/plugins/lua-line.lua
@@ -5,7 +5,7 @@ return {
     require('lualine').setup {
       options = {
         icons_enabled = true,
-        theme = 'dracula',
+        theme = 'auto',
         component_separators = { left = '', right = ''},
         section_separators = { left = '', right = ''},
         disabled_filetypes = {
@@ -14,7 +14,7 @@ return {
         },
         ignore_focus = {},
         always_divide_middle = true,
-        globalstatus = false,
+        globalstatus = true,
         refresh = {
           statusline = 1000,
           tabline = 1000,
