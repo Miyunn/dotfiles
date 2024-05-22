1fe4af82ad27c1414b9cad3d1910d1f010a2348a
Kavinthe Perera <kavintheperera@gmail.com>
2024-05-22 12:55:11 +0530

    copilot support

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,7 +1,7 @@
 return {
-"ewis6991/gitsigns.nvim",
-config = function ()
+  "lewis6991/gitsigns.nvim",
+  config = function ()
     require("gitsigns").setup()
-    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {silent = true})
-end
+    vim.keymap.set("n", "<leader>ph", ":Gitsigns preview_hunk<CR>", {silent = true})
+  end
 }
