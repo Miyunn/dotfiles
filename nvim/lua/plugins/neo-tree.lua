33dac10db4f9de0ff4f162d5d0a08601739d3ea6
Kavinthe Perera <kavintheperera@gmail.com>
2024-05-27 18:20:13 +0530

    Neovim added auto-pair

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -9,6 +9,6 @@ return {
   config = function()
     vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal right<CR>', { silent = true })
     vim.keymap.set('n', '<leader>E', ':Neotree filesystem close<CR>', { silent = true })
-    vim.cmd('Neotree filesystem reveal right')
+    -- vim.cmd('Neotree filesystem reveal right')
   end
 }
