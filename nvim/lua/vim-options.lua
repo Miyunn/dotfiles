9189a16141bc1c634e24f8f2c6d5eda37982ecb6
Kavinthe Perera <kavintheperera@gmail.com>
2024-06-06 23:12:30 +0530

    code folding using tree sitter for neovim

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -3,3 +3,14 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+
+-- Code folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+vim.opt.foldcolumn = "0"
+vim.opt.foldtext = ""
+
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+vim.opt.foldnestmax = 4
