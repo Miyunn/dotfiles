c66437a7578979f8bc885e7b524846998986919d
Kavinthe Perera <kavintheperera@gmail.com>
2024-07-19 02:10:46 +0530

    switched nvim theme to catppuccin (again)

---
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,24 @@
+return {
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    default_integrations = true,
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = false,
+      treesitter = true,
+      notify = false,
+      mini = {
+        enabled = true,
+        indentscope_color = "",
+      },
+    },
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin-macchiato")
+  end,
+}
