aacff8a9a93f55afc5da6e221f0f5c4f33b38dad
Miyunn <kavintheperera@gmail.com>
2025-07-27 20:54:48 +0530

    removes error for loading treesitter before markview.nvim

--- nvim/.config/nvim/lua/plugins/markview.lua
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -1,10 +1,16 @@
 return {
-	"OXY2DEV/markview.nvim",
-	lazy = false, -- Recommended
-	-- ft = "markdown" -- If you decide to lazy-load anyway
+  "OXY2DEV/markview.nvim",
+  lazy = false, -- Recommended
+  -- ft = "markdown" -- If you decide to lazy-load anyway
 
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-tree/nvim-web-devicons",
-	},
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+
+  opts = {
+    experimental = {
+      check_rtp = false,
+    },
+  },
 }
